b60f15ae1f4608c41200535f6b87bedef936ea12
CB2104 <141430425+CB2104@users.noreply.github.com>
2025-04-16 16:15:22 -0300

    actualizacionDeTailwind

--- app/page.js
+++ app/page.js
@@ -4,11 +4,28 @@ import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
 import './globals.css'
 import { Ovo } from 'next/font/google'
+import Header from "./components/Header";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Header/>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <About/>
     </>
   );
 }
